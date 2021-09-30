cc3940a68d35966db65eb159c243c569eb5b10e7
João Alcântara <joaosantalcantara@gmail.com>
2021-09-30 17:32:57 -0300

    text fix

--- pages/index.js
+++ pages/index.js
@@ -353,7 +353,7 @@ export default function Home() {
                 font-normal
 
                 text-[0.765rem]
-                mobilexs:text-[0.6rem]
+                mobilexs:text-[0.65rem]
                 sm:text-lg 
                 md:text-lg 
                 lg:text-xl
